744e85958fab615b565883f654730f16e91ab1d4
Sea_hawk <sea_hawk12@mail.ru>
2024-04-16 10:35:11 +0300

    edit file dbo.udf_GetSKUPrice.sql

--- Function/dbo.udf_GetSKUPrice.sql
+++ Function/dbo.udf_GetSKUPrice.sql
@@ -16,12 +16,12 @@ begin
 			@SumValue decimal(18, 2) = (
 				select sum(b.Value)
 				from dbo.Basket as b
-				where b.ID_SKU = @ID_SKU and b.Value is not null and b.Quantity is not null
+				where b.ID_SKU = @ID_SKU and isnull(b.Value, 0) <> 0 and isnull(b.Quantity, 0) <> 0
 			)
 			,@SumQuantity decimal(18, 2) = (
-				select cast(sum(iif(b.Quantity is not null, b.Quantity, 0)) as decimal(18, 2))
+				select cast(sum(b.Quantity) as decimal(18, 2))
 				from dbo.Basket as b
-				where b.ID_SKU = @ID_SKU and b.Value is not null and b.Quantity is not null
+				where b.ID_SKU = @ID_SKU and isnull(b.Quantity, 0) <> 0
 			)
 
 		if @SumQuantity = 0
