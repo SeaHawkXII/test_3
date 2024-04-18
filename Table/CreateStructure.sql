e6940473293ace7443940e3534b48946c8615423
Sea_hawk <sea_hawk12@mail.ru>
2024-04-18 23:17:09 +0300

    edit file CreateStructure.sql

--- Table/CreateStructure.sql
+++ Table/CreateStructure.sql
@@ -4,7 +4,7 @@ begin
 		ID int not null identity,
 		Code as (concat('s', cast(ID as varchar(255)))) persisted,
 		Name varchar(255),
-		constraint PK_Brand primary key clustered (ID)
+		constraint PK_SKU primary key clustered (ID)
 	)
 	alter table dbo.SKU add constraint UK_SKU_Code unique (Code)
 end
@@ -32,7 +32,7 @@ begin
 		constraint PK_Basket primary key clustered (ID)
 	)
 	alter table dbo.Basket add constraint FK_Basket_ID_SKU_SKU foreign key (ID_SKU) references dbo.SKU(ID)
-	alter table dbo.Basket add constraint FK_Family_ID_Family_Family foreign key (ID_Family) references dbo.Family(ID)
+	alter table dbo.Basket add constraint FK_Basket_ID_Family_Family foreign key (ID_Family) references dbo.Family(ID)
 	alter table dbo.Basket add constraint CK_Basket_Quantity_Value check(Quantity >= 0 and Value >= 0)
-	alter table dbo.Basket add constraint DF_Brand_PurchaseDate default getdate() for PurchaseDate
+	alter table dbo.Basket add constraint DF_Basket_PurchaseDate default getdate() for PurchaseDate
 end
